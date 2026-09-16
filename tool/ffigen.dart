02c0c2da1783d558b69b22036831eb16d1e3f404
Pedro Neto <pedromneto97@gmail.com>
2026-09-16 12:45:32 -0300

    chore(dart): update dependencies

--- tool/ffigen.dart
+++ tool/ffigen.dart
@@ -2,12 +2,20 @@ import 'dart:io';
 
 import 'package:ffigen/ffigen.dart';
 
-void main() {
+Future<void> main() async {
   final packageRoot = Platform.script.resolve('../');
-  FfiGenerator(
-    headers: Headers(entryPoints: [packageRoot.resolve('rust/bindings.h')]),
-    output: Output(dartFile: packageRoot.resolve('lib/src/ffi.g.dart')),
-    functions: Functions.includeSet({'optimize_image_ffi'}),
-    structs: Structs.includeSet({'OptimizeImageOutput'}),
+  final header = packageRoot.resolve('rust/bindings.h');
+
+  await FfiGenerator(
+    input: Input(entryPoints: [header], include: (uri) => uri == header),
+    output: Output(
+      dart: DartOutput(path: packageRoot.resolve('lib/src/ffi.g.dart')),
+    ),
+    visitors: [
+      Visitor(
+        func: (node) => node.isIncluded = node.name == 'optimize_image_ffi',
+        struct: (node) => node.isIncluded = node.name == 'OptimizeImageOutput',
+      ),
+    ],
   ).generate();
 }
